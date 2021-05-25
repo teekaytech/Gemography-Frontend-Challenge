89c9edb7df84c9d0383d4da5beb99343ebb53496
teekaytech <taofeek.olalere@feghas.com>
2021-05-25 13:17:54 +0100

    Add footer component and styles

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import '../stylesheets/footer.scss'
+
+function Footer() {
+  return (
+    <>
+      <p>
+        <a
+          href="https://taofeekolalere.me"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer"
+        >
+          Copyright (c) 2021 Taofeek Abiodun Olalere
+        </a>
+      </p>
+    </>
+  );
+}
+
+export default Footer
--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,12 +1,19 @@
 import '../../stylesheets/App.scss';
+import Footer from '../Footer';
 import RepoList from './RepoList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <RepoList />
+      <header className="App__header">
+        <h1>Most Starred Github Repos created in the last 30 Days</h1>
       </header>
+      <main className="App__main">
+        <RepoList />
+      </main>
+      <footer className="App__footer">
+        <Footer />
+      </footer>
     </div>
   );
 }
