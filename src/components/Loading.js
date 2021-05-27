b52396950d35e306800bfab408e1a4c744ef72dc
teekaytech <taofeek.olalere@feghas.com>
2021-05-27 11:08:27 +0100

    Add and style loading package to enhance loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,17 @@
+import '../stylesheets/loading.scss';
+import ReactLoading from "react-loading";
+
 function Loading() {
   return (
     <section>
-      Loading...
+      <ReactLoading
+        type="spokes"
+        height="7%"
+        width="7%"
+        className="container"
+      />
     </section>
-  )
+  );
 }
 
 export default Loading
--- src/components/Repository.js
+++ src/components/Repository.js
@@ -13,8 +13,8 @@ function Repository({ repo }) {
         <h4 className="name">{repo.name}</h4>
         <p className="desc">{repo.description}</p>
         <p className="meta">
-          <span className="meta__stars">Stars: { repo.stargazers_count }</span>
-          <span className="meta__issues">Issues: {repo.open_issues_count }</span>
+          <span className="meta__stars">{`Stars: ${repo.stargazers_count}`}</span>
+          <span className="meta__issues">{`Issues: ${repo.open_issues_count}`}</span>
           <span className="meta__days">
             {`Submitted ${daysDiff(new Date(repo.created_at))} days ago by ${repo.owner.login}`}
             </span>
